42f6e7eaf50d6370cf315e1b8bec2ee3123de006
Francisco Bultynch <bultynch98@gmail.com>
2024-09-10 21:30:13 -0300

    dividiendo secciones

---
+++ app/Components/Navbar.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { LuLogOut } from "react-icons/lu";
+import { IoSearch } from "react-icons/io5";
+import { MdOutlinePersonOutline, MdAccountBox } from "react-icons/md";
+import DarkModeButton from './DarkModeButton';
+
+function Navbar() {
+
+  return (
+
+    <div className="navbar bg-base-100 py-4 border-b shadowm-sm dark:bg-neutral-800 dark:border-b-neutral-800">
+
+      <div className="navbar-start">
+
+        <div className="dropdown mr-20">
+
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+
+            <div className="indicator">
+              <MdOutlinePersonOutline className='w-6 h-6 dark:text-white' />
+            </div>
+
+          </div>
+
+
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+
+            <li>
+              <Link to='/userProfile' className='p-1 rounded-lg hover:bg-gray-100'>
+                <MdAccountBox className='w-6 h-6'/>
+                Mi cuenta
+              </Link>
+            </li>
+
+            <li>
+              <Link to='/login' className='p-1 rounded-lg hover:bg-gray-100 '>
+                <LuLogOut className='w-6 h-6'/>
+                Cerrar sesión
+              </Link>
+            </li>
+
+          </ul>
+
+        </div>
+
+      </div>
+
+      <div className="navbar-center">
+        <Link to="/" className="btn btn-ghost text-xl hover:rounded-lg dark:text-white dark:hover:bg-neutral-700">
+          <span className="sm:hidden">PC Store</span>
+          <span className="hidden sm:inline">Project Component Store</span>
+        </Link>
+      </div>
+
+
+      <div className="navbar-end flex items-center space-x-2">
+
+        <button className="btn btn-ghost btn-circle dark:hover:bg-neutral-700">
+
+          <IoSearch className='w-5 h-5 dark:text-white' />
+
+        </button>
+
+        <DarkModeButton />
+
+        {/*<Link to='/login' className="btn btn-ghost btn-circle dark:hover:bg-neutral-700">
+
+          <div className="indicator">
+
+            <MdOutlinePersonOutline className='w-6 h-6 dark:text-white' />  
+
+          </div>
+          
+        </Link>
+        */}
+        
+        
+    
+  
+      </div>
+
+    </div>
+
+  );
+}
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,62 @@
-import Image from "next/image";
+import Navbar from './Components/Navbar'
 
 export default function Home() {
   return (
-    <div className="bg-red-400">
+    <div className="bg-violet-900">
 
-      <div className="w-full h-full flex-grow flex items-center justify-center bg-neutral-600 text-white">hola portfolio</div>
+      <div className="p-4 w-full h-full flex-grow flex items-center justify-center bg-violet-950 text-white">
+        <h1 className='text-xl'>BULTYNCH PORTFOLIO</h1>
+      </div>
+
+      <div className='px-4'>
+
+        <div className='my-4 w-full flex flex-col text-white'>
+          <div>
+            <h1 className='text-center'>Presentación</h1>
+          </div>
+
+          <div className='p-2 w-full h-[400px] bg-red-500'>
+            asd
+          </div>
+        </div>
+
+        <div className='my-4 w-full flex flex-col text-white'>
+          <div>
+            <h1 className='text-center'>tecnologias</h1>
+          </div>
+
+          <div className='p-2 w-full h-[400px] bg-yellow-500'>
+            asd
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col text-white'>
+          <div>
+            <h1 className='text-center'>proyectos</h1>
+          </div>
+
+          <div className='p-2 w-full h-[400px] bg-green-500'>
+            asd
+          </div>
+        </div>
+
+        <div className='my-4 w-full flex flex-col text-white'>
+          <div>
+            <h1 className='text-center'>contacto</h1>
+          </div>
+
+          <div className='p-2 w-full h-[400px] bg-yellow-500'>
+            asd
+          </div>
+        </div>
+
+      </div>
+
+      <div className='w-full h-10 bg-violet-950 text-white'>
+        footer
+      </div>
+
+
       
     </div>
   );
